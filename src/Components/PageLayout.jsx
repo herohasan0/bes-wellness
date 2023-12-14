da72205cf259f143a6788b5cd39585596b5249ee
herohasan0 <kahraman.h@outlook.com>
2023-12-14 22:46:41 +0300

    update header and sidebar

--- src/Components/PageLayout.jsx
+++ src/Components/PageLayout.jsx
@@ -1,19 +1,25 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 export default function PageLayout({ children }) {
   return (
     <>
-      <nav className="fixed w-full bg-blue-600 z-40 py-2">
-        <ul className="flex flex-col font-medium pl-2">
-          <li>
-            <a
+      <nav className="fixed w-full z-40 border-b-2 border-solid border-[#D1D5DB]/60 bg-white">
+        <ul className="flex flex-col font-medium">
+          <li className="py-7">
+            <Link
               href="#"
-              className="block py-2 px-3 rounded text-white"
+              className="block rounded text-white"
               aria-current="page"
             >
-              LOGO
-            </a>
+              <div className="relative w-64 h-10 ml-12">
+                <Image
+                  src="https://saglik.anadolusigorta.com.tr/static/media/logo-new.0c5531b6.svg"
+                  fill
+                />
+              </div>
+            </Link>
           </li>
         </ul>
       </nav>
@@ -23,12 +29,12 @@ export default function PageLayout({ children }) {
         className="fixed top-0 left-0 w-64 h-screen shadow-xl"
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 ">
-          <ul className="space-y-2 font-medium pt-16">
+        <div className="h-full pl-5 pt-5 overflow-y-auto border-b-2 border-solid border-[#D1D5DB]/60 bg-white">
+          <ul className="space-y-2 font-medium pt-24">
             <li>
               <Link
                 href="/analysis"
-                className="flex items-center p-2 rounded-lg text-gray-800 hover:text-white hover:bg-gray-700 group"
+                className="flex items-center py-2.5 rounded-lg text-[#797E9F] hover:text-white hover:bg-gray-700 group"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -53,7 +59,7 @@ export default function PageLayout({ children }) {
             <li>
               <Link
                 href="/radiology"
-                className="flex items-center p-2 rounded-lg text-gray-800 hover:text-white  hover:bg-gray-700 group"
+                className="flex items-center py-2.5 rounded-lg text-[#797E9F] hover:text-white  hover:bg-gray-700 group"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -78,7 +84,7 @@ export default function PageLayout({ children }) {
             <li>
               <Link
                 href="/ai-asistant"
-                className="flex items-center p-2 rounded-lg text-gray-800 hover:text-white  hover:bg-gray-700 group"
+                className="flex items-center py-2.5 rounded-lg text-[#797E9F] hover:text-white  hover:bg-gray-700 group"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -103,7 +109,9 @@ export default function PageLayout({ children }) {
           </ul>
         </div>
       </aside>
-      <div className="ml-64 pt-24 px-10">{children}</div>
+      <div className="ml-64 pt-24 px-10 bg-[#f8f8fad9] min-h-screen">
+        {children}
+      </div>
     </>
   );
 }
